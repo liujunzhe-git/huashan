dc37380f3bce0a7177cb376cce7a1e9a677097bb
ljz <ljz@sina.com>
2020-04-12 13:07:01 +0800

    GitTest...add

---
+++ user/src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author JunZhe Liu
+ * @Date 2020/4/12 13:05
+ * @Version 2.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("添加到暂存区,再上传到本地仓库,再克隆下来!!!");
+    }
+}
